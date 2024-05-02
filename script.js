3537c49e2fdf98ee56ae7af2784dd3dbd3b954a3
Chris Jen Ian D. Roa <152456292+Kuswalt@users.noreply.github.com>
2024-05-02 11:11:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//Chris Jen Ian D. Roa BSCS-2B
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (e) {
+        document.getElementById('display').value = 'Error';
+    }
+}
